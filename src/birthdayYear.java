ad59642d92f04d8ec613a777d3255580358b29d6
M-ITConsult <jchoro74@gmail.com>
2023-09-18 09:41:40 +0200

    update

--- src/birthdayYear.java
+++ src/birthdayYear.java
@@ -2,20 +2,27 @@
 import java.util.Scanner;
 
 public class birthdayYear {
+    
     public static void yearsOld() {
         int theYear;
         int result1;
 
         Scanner sc = new Scanner(System.in);
+        String choice;
+        do {
+                System.out.print("Entrez l'année en cours: ");
+                theYear = sc.nextInt();
+                System.out.print("Entrez votre année de naissance: ");
+                result1 = sc.nextInt();
+                int result = theYear - result1;
 
-        System.out.print("Entrez l'année en cours: ");
-        theYear = sc.nextInt();
-        System.out.print("Entrez votre année de naissance: ");
-        result1 = sc.nextInt();
-        int result = theYear - result1;
 
+                System.out.printf("Votre âge est de %s ans%n", result);
 
-        System.out.printf("Votre âge est de %s ans", result);
+                System.out.print("Un autre calcul? y/n ");
+                choice = sc.next();
+            } while(choice.equalsIgnoreCase("y"));
+        System.out.println("GoodBye!");
     }
 
     public static void main(String[] args) {
